76f4b0bc35cc5711920903666853253d1ca84a01
Gareth <blackfireweb@gmail.com>
2023-06-17 01:00:52 +0100

    Added a new level of cannibal funeral to enable consumption of other ideos

--- Source/HarmonyPatches/Patch_IdeoManager.cs
+++ Source/HarmonyPatches/Patch_IdeoManager.cs
@@ -14,15 +14,41 @@ namespace AshAndDust.HarmonyPatches
         public static void Postfix(Pawn pawn)
         {
             if (pawn?.RaceProps?.Humanlike != true) return;
-            if (pawn.Faction == Faction.OfPlayer) return;
             if (pawn.Corpse?.Map?.ParentFaction?.IsPlayer is null or false) return;
-            
+
+            BurialForNonColonist(pawn);
+            CrossIdeoBurial(pawn);
+        }
+
+        public static void BurialForNonColonist(Pawn pawn)
+        {
+            if (pawn.Faction == Faction.OfPlayer) return;
+
             var ideos = Faction.OfPlayer.ideos.AllIdeos.ToList();
             foreach (var ideo in ideos)
             {
                 foreach (var precept in ideo.PreceptsListForReading)
                 {
                     if (Patch_Corpse_PostCorpseDestroy.TryGetPreceptComp<BurialForNonMembers>(precept) is not null)
+                    {
+                        ideo.Notify_MemberDied(pawn);
+                    }
+                }
+            }
+        }
+
+        public static void CrossIdeoBurial(Pawn pawn)
+        {
+            if (pawn.Faction != Faction.OfPlayer) return;
+
+            var ideos = Faction.OfPlayer.ideos.AllIdeos.ToList();
+            foreach (var ideo in ideos)
+            {
+                if (ideo == pawn.ideo.Ideo) continue;
+                
+                foreach (var precept in ideo.PreceptsListForReading)
+                {
+                    if (Patch_Corpse_PostCorpseDestroy.TryGetPreceptComp<CrossIdeoBurial>(precept) is not null)
                     {
                         ideo.Notify_MemberDied(pawn);
                     }
